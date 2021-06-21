005f42b2606517e55d5a366c3abb025e33efed1e
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-06-21 10:41:42 -0300

    criação dos arquivos fragment e dos arquivos de estilização.

--- src/main/java/br/com/java/springbootthymeleafmaster/controller/HomeController.java
+++ src/main/java/br/com/java/springbootthymeleafmaster/controller/HomeController.java
@@ -2,11 +2,19 @@ package br.com.java.springbootthymeleafmaster.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
-    @GetMapping("/")
+   /* @GetMapping("/")
     public String home() {
         return "/home";
+    }*/
+
+    @GetMapping("/home")
+    public ModelAndView home() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("/home");
+        return modelAndView;
     }
 }

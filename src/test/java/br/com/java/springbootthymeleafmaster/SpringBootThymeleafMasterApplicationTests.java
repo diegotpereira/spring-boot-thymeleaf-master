790aa3517aae708619b9a3a3e8693153606916af
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-06-21 09:11:11 -0300

    first commit

---
+++ src/test/java/br/com/java/springbootthymeleafmaster/SpringBootThymeleafMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.java.springbootthymeleafmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootThymeleafMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

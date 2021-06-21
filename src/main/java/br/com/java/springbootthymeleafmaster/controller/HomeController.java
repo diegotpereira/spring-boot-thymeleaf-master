28d38274c8c44fe344d271274cceaa4b022b0cb6
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-06-21 13:55:15 -0300

    alteração arquivos template.

--- src/main/java/br/com/java/springbootthymeleafmaster/controller/HomeController.java
+++ src/main/java/br/com/java/springbootthymeleafmaster/controller/HomeController.java
@@ -1,8 +1,12 @@
 package br.com.java.springbootthymeleafmaster.controller;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.data.domain.Page;
 
 @Controller
 public class HomeController {
@@ -11,9 +15,17 @@ public class HomeController {
         return "/home";
     }*/
 
+    private static final int INITIAL_PAGE = 0;
+
     @GetMapping("/home")
-    public ModelAndView home() {
+    public ModelAndView home(@RequestParam("page") Optional<Integer> page) {
+
+        int evalPage = (page.orElse(0) < 1) ? INITIAL_PAGE : page.get() - 1;
+
+        Page<Product> products = productService.findAllProductsPageable(new PageRequest(evalPage, 5));
+
         ModelAndView modelAndView = new ModelAndView();
+        
         modelAndView.setViewName("/home");
         return modelAndView;
     }

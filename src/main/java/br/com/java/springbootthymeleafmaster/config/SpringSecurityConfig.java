e66aa82d7fe9c54e0c9d48e782ad81ae48fa7e1f
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-06-23 13:30:22 -0300

    alteração nos arquivos classe java.

--- src/main/java/br/com/java/springbootthymeleafmaster/config/SpringSecurityConfig.java
+++ src/main/java/br/com/java/springbootthymeleafmaster/config/SpringSecurityConfig.java
@@ -8,10 +8,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.access.AccessDeniedHandler;
+
 
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
@@ -28,7 +30,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
     @Value("${spring.queries.users-query}")
     private String usersQuery;
 
-    @Value("${spring.queries.users-query}")
+    @Value("${spring.queries.roles-query}")
     private String rolesQuery;
 
     @Autowired
@@ -36,26 +38,29 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
         this.accessDeniedHandler = accessDeniedHandler;
         this.dataSource = dataSource;
     }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
         http.csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/home", "/registration", "/error", "/h2-console/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login")
-                .defaultSuccessUrl("/home")
-                .permitAll()
-                .and()
-                .logout()
-                .permitAll()
-                .and()
-                .exceptionHandling().accessDeniedHandler(accessDeniedHandler)
-                // Fix for H2 console
-                .and().headers().frameOptions().disable();
+            .authorizeRequests()
+            .antMatchers("/home", "/registration", "/error").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .formLogin()
+            .loginPage("/login")
+            .defaultSuccessUrl("/home")
+            .permitAll()
+            .and()
+            .logout()
+            .permitAll()
+            .and()
+            .exceptionHandling().accessDeniedHandler(accessDeniedHandler)
+            .and().headers().frameOptions().disable();
+    }
+    
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/src/main/resources/static/js/**");
     }
 
     @Autowired
---
+++ src/main/java/br/com/java/springbootthymeleafmaster/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package br.com.java.springbootthymeleafmaster.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import br.com.java.springbootthymeleafmaster.model.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Long>{
+
+    Role findByRole(@Param("role") String role);
+}

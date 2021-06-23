5d6bf6c37ff16503a1eaa2bcdb9abdd31ce5dc46
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-06-23 17:06:05 -0300

    alteração no arquivo classe modelo produto adicionado atributos.

--- src/main/java/br/com/java/springbootthymeleafmaster/model/Produto.java
+++ src/main/java/br/com/java/springbootthymeleafmaster/model/Produto.java
@@ -1,5 +1,7 @@
 package br.com.java.springbootthymeleafmaster.model;
 
+import java.math.BigDecimal;
+
 import javax.persistence.*;
 
 @Entity
@@ -10,5 +12,104 @@ public class Produto {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "produto_id")
     private Long id;
+
+    @Column(name = "nome", nullable = false, unique = true)
+    private String nome;
+
+    @Column(name = "preco", nullable = false)
+    private BigDecimal preco;
+
+    @Column(name = "descricao", nullable = false)
+    private String descricao;
+
+    @Column(name = "quantidade", nullable = false)
+    private Integer quantidade;
     
+
+    /**
+     * @return Long return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return String return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return BigDecimal return the preco
+     */
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    /**
+     * @param preco the preco to set
+     */
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    /**
+     * @return String return the descricao
+     */
+    public String getDescricao() {
+        return descricao;
+    }
+
+    /**
+     * @param descricao the descricao to set
+     */
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    /**
+     * @return Integer return the quantidade
+     */
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    /**
+     * @param quantidade the quantidade to set
+     */
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+        Produto produto = (Produto) o;
+
+        return id.equals(produto.id);
+    }
+    
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
 }
